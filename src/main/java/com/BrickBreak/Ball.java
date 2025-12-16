df3c272c66af9d2b6aa51aa82b9d7919f836aa6d
FilipMilicevic09 <filip.milicevic009@gmail.com>
2025-12-16 19:30:13 +0000

    sd

--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,4 +1,4 @@
-//Your name
+//Filip Milicevic
 // a brief description
 
 package com.BrickBreak;
@@ -12,11 +12,29 @@ public class Ball {
 	private int x;
 	private int y;
 	private int size;
+	private int xVelocity;
+	private int yVelocity;
 	
-	Ball (int, int, int)
+	// constructors
+	Ball (int y, int x, int size){
+	this.x = x;
+	this.y = y;
+	this.size = size;
+	xVelocity = 1;
+	yVelocity = 1;
+	}
 	//methods:
 	public void draw(Graphics g){	
-	g.setColor(Color.cyan);
+	g.setColor(Color.red);
 	g.fillOval(x, y, size, size);
+	}
+	public int getXpos(){
+		return  x;
+	}
+	public int getYpos(){
+		return  y;
+	}
+	public in getSize(){
+		return size;
 	}
 }
